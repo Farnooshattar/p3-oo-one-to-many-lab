500fdab5ec7d446b1af3826fa204babfded76c95
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-08-03 03:38:51 -0400

    performs One-to-many Object Relationships

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,26 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all=[]
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.owner=owner
+        if pet_type not in self.PET_TYPES:
+            raise TypeError("pet type not valid")
+        self.pet_type = pet_type
+        Pet.all.append(self)
+        
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name = name
+        
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    def add_pet(self, pet):
+        if pet.pet_type not in Pet.PET_TYPES:
+            raise TypeError("pet type not valid")
+        pet.owner = self
+    def get_sorted_pets(self):
+        pet_collection=[pet for pet in Pet.all if pet.owner == self]
+        pet_collection.sort(key=lambda pet: pet.name)
+        return pet_collection
